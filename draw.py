992f3f70172d25169855971f55166e726c8a0471
alexanderzou <azou00@stuy.edu>
2020-02-07 17:50:34 -0500

    wrote some code

--- draw.py
+++ draw.py
@@ -1,4 +1,18 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
+    if x0 > x1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    m = float(A) / float(B)
+    if m > 0 and m <= 1:
+        #run O1
+    if m > 1:
+        #run O2
+    if m < 0 and m >= -1:
+        #run O8
+    if m < -1:
+        #run O7
     pass
