bfb136b70c618621765779b5b2856c4139bc4e6c
alexanderzou <azou00@stuy.edu>
2020-02-07 21:27:17 -0500

    fixed math and compensated for vertical/horizontal

--- draw.py
+++ draw.py
@@ -5,12 +5,21 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if x0 > x1:
         x0, x1 = x1, x0
         y0, y1 = y1, y0
-    A = y1 - y0
-    B = -1 * (x1 - x0)
-    m = -1 * float(A) / float(B)
-    #print('{}, {}, {}, {}, {}'.format(m, x0, y0, x1, y1))
     x = x0
     y = y0
+    A = y1 - y0
+    if A == 0:
+        while x < x1:
+            plot(screen, color, x, y)
+            x += 1
+    B = -1 * (x1 - x0)
+    if B == 0:
+        while y < y1:
+            plot(screen, color, x, y)
+            y += 1
+        return
+    m = -1 * float(A) / float(B)
+    #print('{}, {}, {}, {}, {}'.format(m, x0, y0, x1, y1))
     if m > 0 and m <= 1:
         #run O1/O5
         d = 2 * A + B
@@ -44,7 +53,9 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if m < -1:
         #run O7/O3
         d = A - (2 * B)
-        while y <= y1:
+        while y >= y1:
+            #print('{}, {}'.format(x,y))
+            plot(screen, color, x, y)
             if d > 0:
                 x += 1
                 d += 2 * A
