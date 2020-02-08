ed3fe4bd01771ddc81fcd1966e4210ad9b83fde1
alexanderzou <azou00@stuy.edu>
2020-02-07 20:46:03 -0500

    O1 & O5 tested

--- draw.py
+++ draw.py
@@ -1,18 +1,34 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
+    #print('hey')
     if x0 > x1:
         x0, x1 = x1, x0
         y0, y1 = y1, y0
     A = y1 - y0
     B = -1 * (x1 - x0)
-    m = float(A) / float(B)
+    m = -1 * float(A) / float(B)
+    #print('{}, {}, {}, {}, {}'.format(m, x0, y0, x1, y1))
     if m > 0 and m <= 1:
-        #run O1
+        #run O1/O5
+        x = x0
+        y = y0
+        d = 2 * A + B
+        while x <= x1:
+            #print('{}, {}'.format(x, y))
+            plot(screen, color, x, y)
+            if d > 0:
+                y += 1
+                d += 2 * B
+            x += 1
+            d += 2 * A
     if m > 1:
-        #run O2
+        #run O2/O6
+        True == True
     if m < 0 and m >= -1:
-        #run O8
+        #run O8/O4
+        True == True
     if m < -1:
-        #run O7
+        #run O7/O3
+        True == True
     pass
