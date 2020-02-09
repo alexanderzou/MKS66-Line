3582cce480ab3473a62550cda1d1debf173a0d8a
alexanderzou <azou00@stuy.edu>
2020-02-08 23:39:04 -0500

    fixed bug

--- draw.py
+++ draw.py
@@ -9,12 +9,16 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     y = y0
     A = y1 - y0
     if A == 0:
-        while x < x1:
+        while x <= x1:
             plot(screen, color, x, y)
             x += 1
     B = -1 * (x1 - x0)
     if B == 0:
-        while y < y1:
+        print('{}, {}'.format(y, y1))
+        if y0 > y1:
+            y = y1
+            y1 = y0
+        while y <= y1:
             plot(screen, color, x, y)
             y += 1
         return
@@ -61,4 +65,3 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d += 2 * A
             y -= 1
             d -= 2 * B
-    pass
